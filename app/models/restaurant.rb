43339c705e445ae3629d34508e29da2b2e9d8a25
nickrdandrea <nickrdandrea@gmail.com>
2017-02-07 16:50:52 -0500

    Fixed associations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
-  belongs_to :owner, class_name "User"
+  belongs_to :owner, class_name: "User"
   belongs_to :category
   has_many :reservations
   has_many :users, through: :reservations
