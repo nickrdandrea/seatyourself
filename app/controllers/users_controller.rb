069a89d544c76c6c0d013816b4993c6094589fa0
yonas samuel <yonastlolo@yahoo.com>
2017-02-08 16:19:39 -0500

    user-controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
       @user = User.new(user_params)
     if @user.save
-        redirect_to_reservation_url, notice: "Signed up"
+        redirect_to_users_url, notice: "Signed up"
     else
         render "new"
     end
