d02b1b497557833d263a01a061e357d4b0a17696
tasconajoey <joey.tascona@gmail.com>
2017-02-08 14:39:34 -0500

    Add more controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  root 'home#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurant
+
+  resources :users
+
+  resources :reservation
+
+  resources :sessions, only: [:new, :create, :destroy]
 end
--- test/controllers/restaurants_controller_test.rb
+++ test/controllers/restaurants_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class CategoriesControllerControllerTest < ActionDispatch::IntegrationTest
+class RestaurantsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
