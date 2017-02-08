1f307dfe1d2cfd8c3d78d8f22109b1fe4cceefee
yonas samuel <yonastlolo@yahoo.com>
2017-02-08 16:07:27 -0500

    user-controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
 
   private
    def user_params
-     params.require(:user).permit(:first_name, last_name :email, :phone :password, :password_confirmation, :username)
+     params.require(:user).permit(:first_name, :last_name :email, :phone :password, :password_confirmation, :username)
    end
   end
  end
