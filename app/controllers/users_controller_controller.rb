37c70605cae869caa320f84bdf50f335f368aa00
yonas samuel <yonastlolo@yahoo.com>
2017-02-08 15:18:56 -0500

    user_controller

--- app/controllers/users_controller_controller.rb
+++ app/controllers/users_controller_controller.rb
@@ -1,2 +1,40 @@
 class UsersControllerController < ApplicationController
+  def new
+      @user = User.new
+  end
+
+  def create
+      @user = User.new(user_params)
+    if @user.save
+        redirect_to_reservation_url, notice: "Signed up"
+    else
+        render "new"
+    end
+  end
+
+   def edit
+     @user = User.find(params[:id])
+   end
+
+   def update
+    @user = User.find(params[:id])
+
+    if @User.update_attributes(product_params)
+      redirect_to_users_url(@user)
+    else
+      render :edit
+    end
+  end
+
+    def destroy
+      @user = User.find(params[:id])
+      @user.destroy
+      redirect_to_users_url
+    end
+
+  private
+   def user_params
+     params.require(:user).permit(:first_name, last_name :email, :phone :password, :password_confirmation, :username)
+   end
+  end
 end
