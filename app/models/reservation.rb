6672236c14efda21d3317e9c1e5f84f2583b4f3c
yonas samuel <yonastlolo@yahoo.com>
2017-02-09 14:29:21 -0500

    Validation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,8 @@
 class Reservation < ApplicationRecord
+  validates :date, :time, presence: true
+  validates :seats, numericality: {only_integer: true}
+
+  
   belongs_to :restaurant
   belongs_to :user
 end
