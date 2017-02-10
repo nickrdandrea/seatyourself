112ed0802633ef2f73ca359f5df173a10053354a
nickrdandrea <nickrdandrea@gmail.com>
2017-02-09 22:13:06 -0500

    Added reservation list to user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @restaurant = Restaurant.where(owner_id: @user.id)
+    @reservation = Reservation.where(user_id: @user.id)
   end
 
    def edit
